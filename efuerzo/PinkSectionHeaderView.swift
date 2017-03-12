555fa11188002c4534465e0ce43d43c10fa3c347
Nouman Mehmood <noumanmehmood@noumans-mbp>
2017-03-12 05:25:30 +0000

    Incorporated calendar view

---
+++ efuerzo/PinkSectionHeaderView.swift
@@ -0,0 +1,15 @@
+//
+//  PinkSectionHeaderView.swift
+//  efuerzo
+//
+//  Created by Nouman Mehmood on 12/03/2017.
+//  Copyright © 2017 Nouman Mehmood. All rights reserved.
+//
+
+import JTAppleCalendar
+
+class PinkSectionHeaderView: JTAppleHeaderView {
+
+    @IBOutlet var title: UILabel!
+
+}
--- efuerzo/TimetableViewController.swift
+++ efuerzo/TimetableViewController.swift
@@ -7,17 +7,132 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 class TimetableViewController: UIViewController {
-
-    @IBOutlet weak var labelPassedData: UILabel!
-    var toPass:String!
     
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    
+    let white = UIColor(colorWithHexValue: 0xECEAED)
+    let darkPurple = UIColor(colorWithHexValue: 0x3A284C)
+    let dimPurple = UIColor(colorWithHexValue: 0x574865)
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        calendarView.dataSource = self
+        calendarView.delegate = self
+        calendarView.registerCellViewXib(file: "CellView") // Registering your cell is manditory
+        calendarView.cellInset = CGPoint(x: 0, y: 0)
+        
+        let doubleTapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didDoubleTapCollectionView(gesture:)))
+        doubleTapGesture.numberOfTapsRequired = 2  // add double tap
+        calendarView.addGestureRecognizer(doubleTapGesture)
+        
+        calendarView.registerHeaderView(xibFileNames: ["PinkSectionHeaderView"])
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    func didDoubleTapCollectionView(gesture: UITapGestureRecognizer) {
+        let point = gesture.location(in: gesture.view!)
+        let cellState = calendarView.cellStatus(at: point)
+        if (cellState != nil){
+            print(cellState!.date)
+        }
+    }
+    
+    // This sets the height of your header
+    func calendar(_ calendar: JTAppleCalendarView, sectionHeaderSizeFor range: (start: Date, end: Date), belongingTo month: Int) -> CGSize {
+        return CGSize(width: 200, height: 50)
+    }
+    // This setups the display of your header
+    func calendar(_ calendar: JTAppleCalendarView, willDisplaySectionHeader header: JTAppleHeaderView, range: (start: Date, end: Date), identifier: String) {
+        let headerCell = (header as? PinkSectionHeaderView)
+        headerCell?.title.text = "S           M           T           W           T           F           S"
+    }
+    
+    // Function to handle the text color of the calendar
+    func handleCellTextColor(view: JTAppleDayCellView?, cellState: CellState) {
+        guard let myCustomCell = view as? CellView  else {
+            return
+        }
+        
+        if cellState.isSelected {
+            myCustomCell.dayLabel.textColor = darkPurple
+        } else {
+            if cellState.dateBelongsTo == .thisMonth {
+                myCustomCell.dayLabel.textColor = white
+            } else {
+                myCustomCell.dayLabel.textColor = dimPurple
+            }
+        }
+    }
+    
+    // Function to handle the calendar selection
+    func handleCellSelection(view: JTAppleDayCellView?, cellState: CellState) {
+        guard let myCustomCell = view as? CellView  else {
+            return
+        }
+        if cellState.isSelected {
+            myCustomCell.selectedView.layer.cornerRadius =  25
+            myCustomCell.selectedView.isHidden = false
+        } else {
+            myCustomCell.selectedView.isHidden = true
+        }
+    }
+
+}
+
+extension TimetableViewController: JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        
+        let startDate = formatter.date(from: "2010 02 01")! // You can use date generated from a formatter
+        let endDate = formatter.date(from: "2110 02 01")!                                // You can also use dates created from this function
+        let calendar = Calendar.current                     // Make sure you set this up to your time zone. We'll just use default here
+        
+        let parameters = ConfigurationParameters(startDate: startDate,
+                                                 endDate: endDate,
+                                                 numberOfRows: 5,
+                                                 calendar: calendar,
+                                                 generateInDates: .forAllMonths,
+                                                 generateOutDates: .tillEndOfGrid,
+                                                 firstDayOfWeek: .sunday)
+        return parameters
+    }
+    
+    
+    
+    func calendar(_ calendar: JTAppleCalendarView, willDisplayCell cell: JTAppleDayCellView, date: Date, cellState: CellState) {
+        let myCustomCell = cell as! CellView
+        
+        // Setup Cell text
+        myCustomCell.dayLabel.text = cellState.text
+        
+        handleCellTextColor(view: cell, cellState: cellState)
+        handleCellSelection(view: cell, cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleDayCellView?, cellState: CellState) {
+        handleCellSelection(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleDayCellView?, cellState: CellState) {
+        handleCellSelection(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+    }
+    
+}
+
+extension UIColor {
+    convenience init(colorWithHexValue value: Int, alpha:CGFloat = 1.0){
+        self.init(
+            red: CGFloat((value & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((value & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(value & 0x0000FF) / 255.0,
+            alpha: alpha
+        )
     }
 }
